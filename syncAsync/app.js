13c38cf9922e84565e5586fca7bff0233fe7a602
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-10-23 14:09:27 +0530

    API Introduction | Fetch | AsyncAwait

--- syncAsync/app.js
+++ syncAsync/app.js
@@ -92,8 +92,71 @@ console.log(makeFoodPromise); // Pending Promise
 
 // API Introduction :
 
+//    An API (Application Programming Interface) is a set of rules and protocols for building software applications.
+// It allows different software systems to communicate with each other.
+// APIs can be used to access web services, databases, and other resources.
+// API calls can be made using various methods, including XMLHttpRequest, Fetch API, and libraries like Axios.
 
-// Fetch API :
 
+// Fetch:
+// Fetch API is a modern interface for making HTTP requests in JavaScript. 
+// It returns Promises and provides a more powerful and flexible feature set than older methods like XMLHttpRequest.
+
+// Syntax :
+    // fetch().then().then().catch();
+
+    const apiEndPoint = "https://dummyjson.com/products";
+
+    fetch(apiEndPoint).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        console.log(data.products);
+        // Do something with the product data
+        const productsContainer = document.getElementById("products");
+        productsContainer.innerHTML = data.products.map((product)=>{
+ 
+        return(
+            `<div class="product-card">
+                <img src="${product.thumbnail}" alt="${product.title}" />
+                <h3>${product.title}</h3>
+                <p>Price: $${product.price}</p>
+            </div>`
+        );
+        }).join("");
+        // data.products.forEach((product)=>{
+        //     console.log(`Product: ${product.title}, Price: $${product.price}`);
+        // });
+    }).catch(
+        (error)=>{
+            console.log("Error fetching data:", error);
+        }
+    );
 
 // Async Await :
+
+    const fetchProducts = async () => {
+
+        const response = await fetch(apiEndPoint);
+        const data = await response.json();
+        console.log(data);
+
+    };
+
+    const btn = document.getElementById("btn");
+    btn.addEventListener("click", fetchProducts);
+
+
+    // Syntax for Native Fetch :
+
+    // fetch("url")
+    // .then((res)=>{res.json()})
+    // .then((data)=>{console.log(data)})
+    // .catch((error)=>{console.log(error)});
+
+    // Syntax for AsynAwait :
+
+    // async function fetchProd(params) {
+    //     const res = await fetch(url);
+    //     const data = await res.json();
+    //     console.log(data);
+    // }
